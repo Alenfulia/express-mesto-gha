ba8185dee6334bba0d8064109537c93b770f0f48
Alenfulia <alfenkaa@yandex.ru>
2022-04-15 00:43:45 +0300

    fix-2_sprint13

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => {
-      res.status(200).send(user);
+      res.status(200).send({ data: user });
     })
     .catch(() => res.status(ERROR_BAD_REQUEST).send({ message: 'Внутренняя ошибка сервера.' }));
 };
@@ -45,8 +45,10 @@ module.exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name, about },
+    { new: true },
   )
-    .then((user) => res.status(200).send(user))
+    .then((user) =>
+      res.status(200).send({ data: user }))
     .catch(() => res.status(ERROR_BAD_REQUEST).send({ message: 'Внутренняя ошибка сервера.' }));
 };
 
@@ -56,7 +58,8 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar },
+    { new: true },
   )
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(200).send({ data: user }))
     .catch(() => res.status(ERROR_BAD_REQUEST).send({ message: 'Внутренняя ошибка сервера.' }));
 };
